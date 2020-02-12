1bca1eb22ea92199ebdf206c1a0d9d9aa551ef83
Vitalii Ananev <an-vitek@ya.ru>
2020-02-12 23:23:51 +0300

    init

---
+++ src/main/java/ru/portfolio/portfolio/PortfolioApplication.java
@@ -0,0 +1,13 @@
+package ru.portfolio.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/portfolio/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.portfolio.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
