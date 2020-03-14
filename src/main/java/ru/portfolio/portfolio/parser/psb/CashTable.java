35ea9dc0526af4a64e7f8beaf56f6c62622e2745
Vitalii Ananev <an-vitek@ya.ru>
2020-03-14 20:36:18 +0300

    refactor CashTable

--- src/main/java/ru/portfolio/portfolio/parser/psb/CashTable.java
+++ src/main/java/ru/portfolio/portfolio/parser/psb/CashTable.java
@@ -3,14 +3,17 @@ package ru.portfolio.portfolio.parser.psb;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.usermodel.Row;
+import ru.portfolio.portfolio.parser.ExcelTable;
+import ru.portfolio.portfolio.parser.TableColumn;
+import ru.portfolio.portfolio.parser.TableColumnDescription;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static ru.portfolio.portfolio.parser.psb.PsbBrokerReport.EMTPY_RANGE;
+import static ru.portfolio.portfolio.parser.psb.CashTable.CashTableHeader.*;
 
 @Slf4j
 public class CashTable {
@@ -19,47 +22,44 @@ public class CashTable {
     @Getter
     private final PsbBrokerReport report;
     @Getter
-    private final List<Row> data = new ArrayList<>();
+    private final List<CashTableRow> data = new ArrayList<>();
 
     public CashTable(PsbBrokerReport report) {
         this.report = report;
-        this.data.addAll(pasreTable(report, TABLE_START_TEXT, TABLE_END_TEXT, 1));
+        this.data.addAll(pasreTable(report));
     }
 
-    private List<Row> pasreTable(PsbBrokerReport report, String tableName, String tableFooterString, int leftColumn) {
-        CellRangeAddress address = report.getTableCellRange(tableName, tableFooterString);
-        if (address == EMTPY_RANGE) {
-            return Collections.emptyList();
-        }
-        List<Row> data = new ArrayList<>();
-        for (int rowNum = address.getFirstRow() + 3; rowNum < address.getLastRow(); rowNum++) {
-            org.apache.poi.ss.usermodel.Row row = report.getSheet().getRow(rowNum);
-            if (row != null) {
-                Row cash = getCash(row, leftColumn);
-                if (cash != null) {
-                    data.add(cash);
-                }
-            }
-        }
-        return data;
+    private List<CashTableRow> pasreTable(PsbBrokerReport report) {
+        ExcelTable table = ExcelTable.of(report.getSheet(), TABLE_START_TEXT, TABLE_END_TEXT, CashTableHeader.class);
+        table.setDataRowOffset(3);
+        return table.isEmpty() ?
+                Collections.emptyList() :
+                table.getData(report.getPath(), CashTable::getCash);
     }
 
-    private Row getCash(org.apache.poi.ss.usermodel.Row row, int leftColumn) {
-        try {
-            return Row.builder()
-                    .section(row.getCell(leftColumn).getStringCellValue())
-                    .value(BigDecimal.valueOf(row.getCell(leftColumn + 8).getNumericCellValue()))
-                    .currency(row.getCell(leftColumn + 7).getStringCellValue())
-                    .build();
-        } catch (Exception e) {
-            log.warn("Не могу распарсить таблицу '{}' в файле {}, строка {}", TABLE_START_TEXT, report.getPath(), row.getRowNum(), e);
-            return null;
+    private static CashTableRow getCash(ExcelTable table, Row row) {
+        return CashTableRow.builder()
+                .section(table.getCell(row, SECTION).getStringCellValue())
+                .value(BigDecimal.valueOf(table.getCell(row, VALUE).getNumericCellValue()))
+                .currency(table.getCell(row, CURRENCY).getStringCellValue())
+                .build();
+    }
+
+    enum CashTableHeader implements TableColumnDescription {
+        SECTION("сектор"),
+        VALUE("плановый исходящий остаток"),
+        CURRENCY("валюта");
+
+        @Getter
+        private final TableColumn column;
+        CashTableHeader(String ... words) {
+            this.column = ru.portfolio.portfolio.parser.TableColumn.of(words);
         }
     }
 
     @Getter
     @Builder
-    public static class Row {
+    static class CashTableRow {
         private String section;
         private BigDecimal value;
         private String currency;
