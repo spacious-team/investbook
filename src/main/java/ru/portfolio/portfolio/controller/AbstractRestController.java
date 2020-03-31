fd37a3aaece406b6787aa006c05ecef1f928d604
Vitalii Ananev <an-vitek@ya.ru>
2020-03-31 21:01:47 +0300

    add LICENSE

--- src/main/java/ru/portfolio/portfolio/controller/AbstractRestController.java
+++ src/main/java/ru/portfolio/portfolio/controller/AbstractRestController.java
@@ -1,3 +1,21 @@
+/*
+ * Portfolio
+ * Copyright (C) 2020  Vitalii Ananev <an-vitek@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ru.portfolio.portfolio.controller;
 
 import lombok.RequiredArgsConstructor;
@@ -22,7 +40,7 @@ public abstract class AbstractRestController<ID, Pojo, Entity> {
 
     /**
      * Get the entity.
-     * If entity not exists NOT_FOUND http status will be retuned.
+     * If entity not exists NOT_FOUND http status will be returned.
      */
     public ResponseEntity<Entity> get(ID id) {
         return getById(id)
