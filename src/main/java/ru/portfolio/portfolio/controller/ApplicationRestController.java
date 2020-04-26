8cd66c661f9aed1f2c57f766517ed1accdd9add3
Vitalii Ananev <an-vitek@ya.ru>
2020-04-26 23:08:48 +0300

    show application version in index.html, add thymeleaf and actuator dependencies

---
+++ src/main/java/ru/portfolio/portfolio/controller/ApplicationRestController.java
@@ -0,0 +1,38 @@
+/*
+ * Portfolio
+ * Copyright (C) 2020  Vitalii Ananev <an-vitek@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ru.portfolio.portfolio.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ApplicationRestController {
+
+    private final BuildProperties buildProperties;
+
+    @GetMapping("/")
+    public String get(Model model) {
+        model.addAttribute("buildProperties", buildProperties);
+        return "index";
+    }
+}
