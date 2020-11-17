df0e552aec3f56bf1dfd6e8d1f84a4ccd6aca3d9
Vitalii Ananev <an-vitek@ya.ru>
2020-11-18 00:09:56 +0300

    update stock market sheet zoom

--- src/main/java/ru/investbook/view/excel/StockMarketProfitExcelTableView.java
+++ src/main/java/ru/investbook/view/excel/StockMarketProfitExcelTableView.java
@@ -93,7 +93,7 @@ public class StockMarketProfitExcelTableView extends ExcelTableView {
             // Брокеры являются агентами по акциям отечественных бумаг на мосбирже
             sheet.setColumnHidden(TAX_LIABILITY.ordinal(), true); // нет обязательств
         } else {
-            sheet.setZoom(88); // show all columns for 24 inch monitor for securities sheet
+            sheet.setZoom(89); // show all columns for 24 inch monitor for securities sheet
         }
     }
 
