10550959bf7e5f1b22e9fd35b2f2f3947251c584
Vitalii Ananev <an-vitek@ya.ru>
2021-05-02 15:54:10 +0300

    refactor SecurityQuoteTable

--- src/main/java/ru/investbook/parser/uralsib/SecurityQuoteTable.java
+++ src/main/java/ru/investbook/parser/uralsib/SecurityQuoteTable.java
@@ -19,6 +19,7 @@
 package ru.investbook.parser.uralsib;
 
 import org.spacious_team.broker.pojo.SecurityQuote;
+import org.spacious_team.broker.pojo.SecurityQuote.SecurityQuoteBuilder;
 import org.spacious_team.table_wrapper.api.TableRow;
 import ru.investbook.parser.SingleAbstractReportTable;
 
@@ -26,9 +27,13 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import static java.math.RoundingMode.HALF_UP;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static ru.investbook.parser.uralsib.SecuritiesTable.SecuritiesTableHeader.*;
+import static ru.investbook.report.ForeignExchangeRateService.RUB;
 
 public class SecurityQuoteTable extends SingleAbstractReportTable<SecurityQuote> {
 
@@ -57,80 +62,77 @@ public class SecurityQuoteTable extends SingleAbstractReportTable<SecurityQuote>
         BigDecimal quote = row.getBigDecimalCellValue(QUOTE);
         BigDecimal accruedInterest = row.getBigDecimalCellValue(ACCRUED_INTEREST); // в валюте для валютных облигаций
         String isin = row.getStringCellValue(ISIN);
-        Instant reportEndDateTime = getReport().getReportEndDateTime();
 
-        boolean isShare = false;
-        BigDecimal price = null;
-        String currency = null;
+        SecurityQuoteBuilder builder = getShareQuote(quote, priceInRub, accruedInterest)
+                .or(() -> getBondQuote(quote, priceInRub, accruedInterest, amountInRub))
+                .orElseThrow(() -> new IllegalArgumentException("Не смогли вычислить валюту облигации " + isin + ", " +
+                        "цена и НКД могут быть в разных валютах"));
 
-        if (accruedInterest.compareTo(minValue) < 0) { // акция или облигация с НКД == 0 ?
+        return builder
+                .security(isin)
+                .timestamp(getReport().getReportEndDateTime())
+                .build();
+    }
+
+    private Optional<SecurityQuoteBuilder> getShareQuote(BigDecimal quote, BigDecimal priceInRub, BigDecimal accruedInterest) {
+        if (accruedInterest.compareTo(minValue) < 0) { // акция или облигация с НКД == 0
             if (priceInRub.subtract(quote).abs().compareTo(minValue) < 0) {
                 // акция, котировка в руб
-                isShare = true;
-                currency = "RUB";
-                price = null;
-                accruedInterest = null;
+                return of(SecurityQuote.builder()
+                        .quote(quote)
+                        .currency(RUB));
             } else {
+                Instant reportEndDateTime = getReport().getReportEndDateTime();
                 for (String tryCurrency : currencies) {
                     try {
-                        BigDecimal rate = foreignExchangeRateTable.getExchangeRate(tryCurrency, "RUB", reportEndDateTime);
+                        BigDecimal rate = foreignExchangeRateTable
+                                .getExchangeRate(tryCurrency, RUB, reportEndDateTime);
                         if (priceInRub.divide(rate, 2, HALF_UP).subtract(quote).abs().compareTo(minValue) < 0) {
                             // акция (Tesla, Apple), котировка в иностранной валюте
-                            isShare = true;
-                            currency = tryCurrency;
-                            price = null;
-                            accruedInterest = null;
-                            break;
+                            return of(SecurityQuote.builder()
+                                    .quote(quote)
+                                    .currency(tryCurrency));
                         }
                     } catch (Exception ignore) {
                     }
                 }
             }
         }
+        return empty();
+    }
 
-        if (!isShare) {
-            // Облигация. Цена в отчете в рублях, НКД может быть в валюте
-            BigDecimal totalFaceValueInRub = amountInRub.multiply(HUNDRED).divide(quote, 2, HALF_UP); // номинал в рублях
-            boolean isRubBond = false;
+    private Optional<SecurityQuoteBuilder> getBondQuote(BigDecimal quote, BigDecimal priceInRub, BigDecimal accruedInterest,
+                                                        BigDecimal amountInRub) {
+        // Цена в отчете в рублях, НКД может быть в валюте, если так, надо привести цену к валюте
+        BigDecimal totalFaceValueInRub = amountInRub.multiply(HUNDRED).divide(quote, 2, HALF_UP); // номинал в рублях
+        try {
+            int exactTotalFaceValueInRub = totalFaceValueInRub.intValueExact();
+            if (exactTotalFaceValueInRub % 1000 == 0 || exactTotalFaceValueInRub % 100 == 0) {
+                // скорее всего рублевая облигация
+                return of(SecurityQuote.builder()
+                        .quote(quote)
+                        .price(priceInRub)
+                        .accruedInterest(accruedInterest)
+                        .currency(RUB));
+            }
+        } catch (Exception ignore) {
+        }
+
+        Instant reportEndDateTime = getReport().getReportEndDateTime();
+        for (String tryCurrency : currencies) {
             try {
-                int exactTotalFaceValueInRub = totalFaceValueInRub.intValueExact();
-                if (exactTotalFaceValueInRub % 1000 == 0 || exactTotalFaceValueInRub % 100 == 0) {
-                    // скорее всего рублевая облигация
-                    isRubBond = true;
-                    currency = "RUB";
-                    price = priceInRub;
-                    // НКД уже в рублях
-                }
+                BigDecimal rate = foreignExchangeRateTable
+                        .getExchangeRate(tryCurrency, RUB, reportEndDateTime);
+                totalFaceValueInRub.divide(rate, 2, HALF_UP).intValueExact();
+                // не было исключения, облигация в иностранной валюте, приводим цену к валюте
+                return of(SecurityQuote.builder()
+                        .quote(quote)
+                        .price(priceInRub.divide(rate, 4, HALF_UP))
+                        .accruedInterest(accruedInterest)
+                        .currency(tryCurrency));
             } catch (Exception ignore) {
             }
-
-            if (!isRubBond) {
-                for (String tryCurrency : currencies) {
-                    try {
-                        BigDecimal rate = foreignExchangeRateTable.getExchangeRate(tryCurrency, "RUB", reportEndDateTime);
-                        totalFaceValueInRub.divide(rate, 2, HALF_UP).intValueExact();
-                        // облигация в иностранной валюте, приводим цену к валюте
-                        currency = tryCurrency;
-                        price = priceInRub.divide(rate, 4, HALF_UP);
-                        // НКД уже в валюте
-                        break;
-                    } catch (Exception ignore) {
-                    }
-                }
-            }
-
-            if (currency == null && accruedInterest.floatValue() > 0.01) {
-                throw new IllegalArgumentException("Не смогли вычислить валюту облигации " + isin +
-                        " , цена и НКД могут быть в разных валютах");
-            }
         }
-        return SecurityQuote.builder()
-                .security(isin)
-                .timestamp(reportEndDateTime)
-                .quote(quote)
-                .price(price)
-                .accruedInterest(accruedInterest)
-                .currency(currency)
-                .build();
+        return empty();
     }
 }
