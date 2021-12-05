88652dff8ab14eca6a66f74ddaba1a6aec0bda90
Vitalii Ananev <an-vitek@ya.ru>
2021-12-05 22:49:06 +0300

    add security.type column

---
+++ src/main/resources/db/migration/all/V2021_11_0_0.sql
@@ -0,0 +1,38 @@
+/*
+ * InvestBook
+ * Copyright (C) 2021  Vitalii Ananev <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+CREATE TABLE IF NOT EXISTS `security_type` (
+    `id`   int(2) UNSIGNED NOT NULL,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Тип ценных бумаг';
+
+INSERT INTO `security_type` (`id`, `name`)
+VALUES (0, 'Акция, ETF'),
+       (1, 'Облигация'),
+       (2, 'Акция или облигация (до выплаты купона, амортизации точно не известно)'),
+       (3, 'Срочный контракт'),
+       (4, 'Валютная пара'),
+       (5, 'Произвольный актив');
+
+ALTER TABLE `security`
+    ADD COLUMN `type` INT(2) UNSIGNED NOT NULL COMMENT 'Тип ценной бумаги' AFTER `id`;
+
+ALTER TABLE `security` ADD KEY `security_type_ix` (`type`);
+ALTER TABLE `security` ADD CONSTRAINT `security_type_fkey`
+    FOREIGN KEY (`type`) REFERENCES `security_type` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT;
