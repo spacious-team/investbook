243a953ffafb80d6582cbf390af5a6def55d4ab3
Vitalii Ananev <an-vitek@ya.ru>
2022-02-02 23:10:58 +0300

    add openformat package skel

---
+++ src/main/java/ru/investbook/openformat/PortfolioOpenFormatRestController.java
@@ -0,0 +1,35 @@
+/*
+ * InvestBook
+ * Copyright (C) 2022  Vitalii Ananev <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ru.investbook.openformat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.investbook.openformat.v1_0_0.PortfolioOpenFormatV1_0_0;
+
+@RestController
+@RequestMapping("/portfolio-open-format")
+public class PortfolioOpenFormatRestController {
+
+    @GetMapping
+    public PortfolioOpenFormatV1_0_0 get() {
+        return PortfolioOpenFormatV1_0_0.builder()
+                .build();
+    }
+}
