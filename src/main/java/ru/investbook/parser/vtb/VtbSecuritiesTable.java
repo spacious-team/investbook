73ecd960047cac7e9769e8d72ab1e6ab7e08fdc9
aakatov <artem.akatov@gmail.com>
2022-02-05 12:44:42 +0300

    fix vtb coupon error

--- src/main/java/ru/investbook/parser/vtb/VtbSecuritiesTable.java
+++ src/main/java/ru/investbook/parser/vtb/VtbSecuritiesTable.java
@@ -53,7 +53,7 @@ public class VtbSecuritiesTable extends SingleAbstractReportTable<Security> {
         String description = row.getStringCellValue(NAME_REGNUMBER_ISIN);
         Security security = VtbReportHelper.getSecurity(description);
         int securityId = getReport().getSecurityRegistrar().declareStockOrBond(security.getIsin(), security::toBuilder);
-        security = Security.builder().id(securityId).build();
+        security = security.toBuilder().id(securityId).build();
         String registrationNumber = description.split(",")[1].toUpperCase().trim();
         regNumberToSecurity.put(registrationNumber, security);
         return security;
