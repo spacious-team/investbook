de692ba0c03192b824365ba0b14d0335cbe0a0fa
Vitalii Ananev <an-vitek@ya.ru>
2022-05-12 16:55:05 +0300

    update copyright

--- src/main/java/ru/investbook/parser/ReportTablesCachingWrapper.java
+++ src/main/java/ru/investbook/parser/ReportTablesCachingWrapper.java
@@ -1,18 +1,18 @@
 /*
  * InvestBook
- * Copyright (C) 2021  Vitalii Ananev <spacious-team@ya.ru>
+ * Copyright (C) 2022  Spacious Team <spacious-team@ya.ru>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General private final License as
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY(); without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General private final License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General private final License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
