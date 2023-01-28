e83cab461ae80234f5bf3f7102e32d65f0a20a34
Vitalii Ananev <an-vitek@ya.ru>
2023-01-28 21:20:41 +0300

    export sql data only for 2022.9 version

--- src/main/java/ru/investbook/upgrade/SqlDataExporter.java
+++ src/main/java/ru/investbook/upgrade/SqlDataExporter.java
@@ -20,29 +20,45 @@ package ru.investbook.upgrade;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import ru.investbook.InvestbookProperties;
 
 import javax.annotation.PreDestroy;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SqlDataExporter {
-    public static final String file = "~/investbook/export-2022.9.sql";
+    private static final String EXPECTED_INVESTBOOK_VERSION_FOR_EXPORT = "2022.9";
+    private final BuildProperties buildProperties;
+    private final InvestbookProperties investbookProperties;
     private final JdbcTemplate jdbcTemplate;
 
     @PreDestroy
-    public void export() {
+    public void preDestroy() {
+        String version = buildProperties.getVersion();
+        if (Objects.equals(version, EXPECTED_INVESTBOOK_VERSION_FOR_EXPORT)) {
+            Path file = investbookProperties.getDataPath()
+                    .resolve("export-" + version + ".sql")
+                    .toAbsolutePath();
+            exportSqlData(file);
+        }
+    }
+
+    private void exportSqlData(Path file) {
         try {
             Instant t0 = Instant.now();
             jdbcTemplate.execute("SCRIPT DROP TO '" + file + "' CHARSET 'UTF-8'");
             Instant t1 = Instant.now();
-            log.info("Экспорт БД для импорта в Investbook 2023.1 завершен за {}", Duration.between(t0, t1));
+            log.info("Экспорт БД в файл '{}' завершен за {}", file, Duration.between(t0, t1));
         } catch (Exception e) {
-            log.error("Не смог выполнить экспорт БД для импорта в Investbook 2023.1", e);
+            log.error("Не смог выполнить экспорт БД в файл '{}'", file, e);
         }
     }
 }
