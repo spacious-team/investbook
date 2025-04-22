f9d530fa4a1472f0e657d120a7511c9b0644fbd7
vananiev <vananiev@users.noreply.github.com>
2025-04-22 23:09:33 +0300

    fix sql-import-files property for debug purpose

--- src/main/java/ru/investbook/InvestbookProperties.java
+++ src/main/java/ru/investbook/InvestbookProperties.java
@@ -20,6 +20,9 @@ package ru.investbook;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.checkerframework.checker.nullness.qual.EnsuresNonNull;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -28,19 +31,19 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 
+import static java.util.Objects.requireNonNull;
+
 @Getter
 @Setter
 @Component
 @ConfigurationProperties("investbook")
-public class InvestbookProperties {
+public class InvestbookProperties implements InitializingBean {
 
     private Path dataPath = Paths.get(System.getProperty("user.home", ""), "investbook");
 
     private Path reportBackupPath = dataPath.resolve("report-backups");
 
-    private List<Path> sqlImportFiles = List.of(
-            dataPath.resolve("export-2022.9.sql"),
-            dataPath.resolve("export-2024.1.x.sql"));
+    private @MonotonicNonNull List<Path> sqlImportFiles;
 
     private boolean openHomePageAfterStart = false;
 
@@ -53,4 +56,19 @@ public class InvestbookProperties {
     private Set<String> tableParsers = Set.of();
 
     private boolean reportBackup = true;
+
+    public List<Path> getSqlImportFiles() {
+        return requireNonNull(sqlImportFiles);
+    }
+
+    @Override
+    @EnsuresNonNull("sqlImportFiles")
+    public void afterPropertiesSet() {
+        if (sqlImportFiles == null) {
+            // lazy initialize. dataPath is configured at this point
+            sqlImportFiles = List.of(
+                    dataPath.resolve("export-2022.9.sql"),
+                    dataPath.resolve("export-2024.1.x.sql"));
+        }
+    }
 }
