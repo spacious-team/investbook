4865541a2405b6a6815e2f6685206b84ebe0bfb2
vananiev <vananiev@users.noreply.github.com>
2025-06-10 02:37:08 +0300

    fix start.bat

--- src/main/assembly/zip/start.bat
+++ src/main/assembly/zip/start.bat
@@ -1,25 +1,25 @@
-#
-# InvestBook
-# Copyright (C) 2023  Spacious Team <spacious-team@ya.ru>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+::
+:: InvestBook
+:: Copyright (C) 2023  Spacious Team <spacious-team@ya.ru>
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU Affero General Public License as
+:: published by the Free Software Foundation, either version 3 of the
+:: License, or (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU Affero General Public License for more details.
+::
+:: You should have received a copy of the GNU Affero General Public License
+:: along with this program.  If not, see <https://www.gnu.org/licenses/>.
+::
 
-# Задать путь к распакованному архиву с Java
-#set JAVA_HOME=C:\Program Files\Java\jdk-24
+:: Задать путь к распакованному архиву с Java
+::set JAVA_HOME=C:\Program Files\Java\jdk-24
 
-# Запуск приложения
+:: Запуск приложения
 chcp 65001
 set PATH=%JAVA_HOME%\bin;%PATH%
 for %%A in (*.jar) do set jarfile=%%A
