0ea852ad3e3fa8561866a2c9aabdbc92d2274c27
Sreeja Khan <voicenote8lifeline@gmail.com>
2025-10-10 19:49:45 +0530

    authentication part 1

---
+++ src/main/java/ru/investbook/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package ru.investbook.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String username;
+}
